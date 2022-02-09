f84262d326bd5bd3c718e9bbf38a5651a1161eea
Sapphire-Moonset <steven.shao6174@gmail.com>
2022-02-10 04:05:12 +0800

    Initial commit

---
+++ wordle/footnote.py
@@ -0,0 +1,16 @@
+import sys
+
+sys.path.append('./')
+sys.path.append('../')
+from manim_imports_ext import *
+from scenes import *
+
+
+class HowWordleColoringWorks(Scene):
+    def construct(self):
+        pass
+
+
+class HowBruteForcingWorks(Scene):
+    def construct(self):
+        pass
